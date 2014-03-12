7b73966212653298d6ce3a78740ada07ea565b1c
Cheezmeister <@gmail>
2014-03-11 18:41:19 -0700

    Safety commit

---
+++ assets/model.js
@@ -0,0 +1,36 @@
+(function() {
+  var root = this;
+  
+  var Fantaskulous = root.Fantaskulous = {};
+  
+  var Priority = {
+      HIGH:   'B',
+      MEDIUM: 'C'
+      LOW:    'D'
+  };
+  
+  var Task = Fantaskulous.Task = {
+    make: function make(params) {
+      var task = {
+        description: params.description || '<Do stuff>',
+        priority: params.priority || 'MEDIUM',
+        completed: params.completed || false,
+        guid: params.guid || guid(),
+        blocks: params.blocks || [],
+        depends: params.depends || []
+      };
+      
+      task.toTodoTxt = function toTodoTxt() {
+        var priority = '(' + this.priority + ')';
+        var comlete = this.complete ? 'x ' : '';
+        var desc = this.description;
+        var guid = 'guid:' + this.guid;
+        var blocks = 'blocks:' + this.blocks.join(',');
+        var depends = 'depends:' + this.depends.join(',');
+        
+        return [complete priority, desc, guid, blocks, depends].join(' ');
+      }
+      return task;
+    }
+  };
+})();
